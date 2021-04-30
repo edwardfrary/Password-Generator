72c8042b6b4476aab34be3ef64359153f450f568
edwardfrary <edwardfrary@gmail.com>
2021-04-30 19:44:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-var res ="";
+var res = "";
+var passWord = "";
 var lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericString = "0123456789";
 var specialString = " !#$%&()*+,-./:;<=>?@[]^_`{|}~";
-
-// Assignment code here
-function randomNumber(){
-  return Math.floor(Math.random()*26)+1;
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -16,26 +12,37 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var passLength = prompt("How long is the password?");
 
-  var lowerCase = prompt("Include lowercase? (Y/N)");
-  if (lowerCase === "Y" || lowerCase === "y" || lowerCase === "YES"){
-  res = res.concat(lowerCaseString);
-  }
+  if (passLength >= 8 && passLength <= 128) {
+    var lowerCase = prompt("Include lowercase? (Y/N)");
+    if (lowerCase === "Y" || lowerCase === "y" || lowerCase === "YES") {
+      res = res.concat(lowerCaseString);
+    }
 
-  var upperCase = prompt("Include uppercase (Y/N)");
-  if (upperCase === "Y" || upperCase === "y" || upperCase === "YES"){
-    res = res.concat(upperCaseString);
-  }
+    var upperCase = prompt("Include uppercase (Y/N)");
+    if (upperCase === "Y" || upperCase === "y" || upperCase === "YES") {
+      res = res.concat(upperCaseString);
+    }
 
-  var numericCharacters = prompt("Include numeric characters? (Y/N)");
-  if (numericCharacters === "Y" || numericCharacters === "y" || numericCharacters === "YES"){
-    res = res.concat(numericString);
-  }
+    var numericCharacters = prompt("Include numeric characters? (Y/N)");
+    if (numericCharacters === "Y" || numericCharacters === "y" || numericCharacters === "YES") {
+      res = res.concat(numericString);
+    }
 
-  var specialCharacters = prompt("Include special characters? (Y/N)");
-  if (specialCharacters === "Y" || specialCharacters === "y" || specialCharacters === "YES"){
-    res = res.concat(specialString);
+    var specialCharacters = prompt("Include special characters? (Y/N)");
+    if (specialCharacters === "Y" || specialCharacters === "y" || specialCharacters === "YES") {
+      res = res.concat(specialString);
+    }
+    console.log(res);
+
+    for (i = 0; i < passLength; i++) {
+      passWord += res.charAt(Math.floor(Math.random() * res.length));
+    }
+
+    console.log(passWord);
+    document.getElementById("password").innerHTML = passWord;
+    res = "";
+    passWord = "";
   }
-  console.log(res);
 }
 
 // Add event listener to generate button
