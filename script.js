962fffa6e0d704bf3126fb1afce94d5c3b69f8fb
edwardfrary <edwardfrary@gmail.com>
2021-04-30 19:18:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Assignment code here
+var res ="";
+var lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+// Assignment code here
+function randomNumber(){
+  return Math.floor(Math.random()*26)+1;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,11 +14,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var passLength = prompt("How long is the password?");
   var lowerCase = prompt("Include lowercase? (Y/N)");
+  if (lowerCase === "Y" || lowerCase === "y" || lowerCase === "YES"){
+  res = res.concat(lowerCaseString);
+  }
   var upperCase = prompt("Include uppercase (Y/N)");
   var numericCharacters = prompt("Include numeric characters? (Y/N)");
   var specialCharacters = prompt("Include special characters? (Y/N)");
 
-  
+
+  for (i=0;i <= passLength-1; i++){
+    
+  }
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
