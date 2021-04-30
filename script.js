71f0536459d3c8fea6ca610d485613744cb5cd42
edwardfrary <edwardfrary@gmail.com>
2021-04-30 18:49:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passLength = prompt("How long is the password?");
+  var lowerCase = prompt("Include lowercase? (Y/N)");
+  var upperCase = prompt("Include uppercase (Y/N)");
+  var numericCharacters = prompt("Include numeric characters? (Y/N)");
+  var specialCharacters = prompt("Include special characters? (Y/N)");
+
+  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
